8064f672b6e6affbcb177e1af474df6466da6bde
ItsThankK <tkingsway3@gmail.com>
2022-10-09 10:35:55 +0100

    commit_1

---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter a number :"))
+op = (input("Enter an operator, do yo want to add, substract, multiply or divide? :"))
+num2 =float(input("Enter another number :"))
+
+if op == "+":
+     result = num1 + num2
+elif op == "-":
+     result = num1 - num2
+elif op == "/":
+     result = num1 / num2
+elif op == "*":
+     result = num1 * num2
+
+
+print(result)
+print ("That's your result!")
